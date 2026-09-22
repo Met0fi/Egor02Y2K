ba2081f7f03924986200a8081663cd808d86882d
Metofian <kenijin2209@gmail.com>
2026-09-22 22:39:45 +0700

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function () {
-  var VISIT_KEY = "egor02_visit";
+  var VISIT_KEY = "egor02_visits";
   var SECRET_KEY = "egor02_secrets";
   var GUEST_KEY = "egor02_guestbook";
   var KEYS_KEY = "egor02_keys";
