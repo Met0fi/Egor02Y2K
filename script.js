42ff65acf483f84718d58a1aa57c9d3e7eae8138
Metofian <kenijin2209@gmail.com>
2026-09-23 21:03:58 +0700

    Убрать текстовые поля записи

--- script.js
+++ script.js
@@ -13,9 +13,9 @@
     { id: "p4", name: "EGOR02", text: "я потом доделаю. не трогайте файлы в cgi-bin", date: "04.08.2004" },
     {
       id: "p5",
-      name: "To4hnoNeDroft",
+      name: "",
       text: "",
-      date: "22.09.2026",
+      date: "",
       img: "guest/guest-aura.png",
       alt: ""
     }
