0429f75f53649796739933c12f3ba0f388e6a443
Metofian <kenijin2209@gmail.com>
2026-09-25 10:25:07 +0700

    .

--- cake-src.js
+++ cake-src.js
@@ -1,12 +1,7 @@
 (function () {
   var img = document.getElementById("cakePic");
   if (!img) return;
-  var files = [];
-  for (var i = 0; i < 13; i++) files.push("assets/p" + (i < 10 ? "0" + i : i) + ".txt");
-  Promise.all(files.map(function (p) {
-    return fetch(p).then(function (r) { return r.text();
-    });
-  })).then(function (parts) {
-    img.src = "data:image/jpeg;base64," + parts.join("").replace(/\s+/g, "");
+  fetch("assets/cake.txt").then(function (r) { return r.text(); }).then(function (t) {
+    img.src = "data:image/jpeg;base64," + t.replace(/\s+/g, "");
   });
 })();
