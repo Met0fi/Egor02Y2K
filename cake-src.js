ac58d2b6a67b559a6d78710a324d54524b64c998
Metofian <kenijin2209@gmail.com>
2026-09-25 10:18:48 +0700

    .

--- cake-src.js
+++ cake-src.js
@@ -1,5 +1,9 @@
 (function () {
   var img = document.getElementById("cakePic");
   if (!img) return;
-  img.src = "assets/cake.jpg";
+  Promise.all(["assets/k0.txt", "assets/k1.txt"].map(function (p) {
+    return fetch(p).then(function (r) { return r.text(); });
+  })).then(function (parts) {
+    img.src = "data:image/jpeg;base64," + parts.join("").replace(/\s+/g, "");
+  });
 })();
